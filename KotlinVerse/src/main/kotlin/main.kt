ceeeb9e9eff56d158c82523114565b69f58eeb81
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-07-05 01:29:49 +0900

    added Kotlin main function

---
+++ KotlinVerse/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Hello, KotlinVerse!")
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    println(a+b)
+    println(7.floorDiv(4))
+    println(7.mod(4))
+}
